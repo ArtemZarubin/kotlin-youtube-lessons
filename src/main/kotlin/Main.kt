a11261e845d8c67aa944eb3aa70022d1092cb247
Artem Zarubin <zarubinartem652@gmail.com>
2023-12-27 01:25:10 +0400

    Explore Variables and Data Types
     Implemented and explored a range of variables, including gym-related metrics such as bench press max and gym name. Demonstrated the versatility of variables through reassignment, specifically showcasing the updated bench press max. Delved into various data types, covering integers, real numbers, strings, characters, and booleans. Detailed comments provide insight into the sizes and characteristics of each data type. Utilized print statements to exhibit practical usage of the variables and their values. Maintained repository cleanliness by concealing the .gitignore file.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,35 @@
 //Lesson 1: Getting started
 fun main() {
-    println("Hello World!")
+    // This is comment
+    // println("Hello World!")
+
+    // Lesson 2: Variables
+    var benchPressMax: Int = 80 // KG of course
+    val nameOfTheGym = "Olympia"
+
+    println("I bench $benchPressMax kg at gym $nameOfTheGym!")
+
+    benchPressMax = 100
+    println("Now I can easily bench $benchPressMax")    // Everything is true
+
+    // Integers
+    val longNum: Long = 7620133724512521551 // 64 bit
+    val intNum: Int = 247344274 // 32 bit
+    val shortNum: Short = 4562  //  16 bit
+    val byteNum: Byte = 127 // 8 bit
+    val unsignedInt: UInt = 7620U   // Favorite number, and also it's only positive values with such type
+
+    // Real numbers
+    val doubleNum: Double = 3.1415926535    // 64 bit
+    val floatNum: Float = 56.78f    // 32 bit
+
+    // Strings and characters
+    val stringOfLetters: String = "abcdefghijklmnopqrstuvwxyz"
+    val onlyOneChar: Char = 'W'
+
+    // Boolean
+    val doYouBelieveMe:Boolean = true   // Which means 'yes'
+    val itsPython: Boolean = false  // No, its Kotlin
 }
+
+
